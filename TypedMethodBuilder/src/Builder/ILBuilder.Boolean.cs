215fd6bfc9dcd6b3bfdb66e293ca14a87f376144
acple <silentsphere110@gmail.com>
2018-08-04 19:12:38 +0900

    Add compare operators

--- TypedMethodBuilder/src/Builder/ILBuilder.Boolean.cs
+++ TypedMethodBuilder/src/Builder/ILBuilder.Boolean.cs
@@ -31,5 +31,39 @@ namespace TypedMethodBuilder
             where TCallStack : ITypeList
             where T : struct
             => new IL<TParameter, TLocal, Stack<T, TCallStack>>(new Op(OpCodes.Not), il);
+
+        public static IL<TParameter, TLocal, Stack<bool, TCallStack>> Ceq<T, TParameter, TLocal, TCallStack>(this IL<TParameter, TLocal, Stack<T, Stack<T, TCallStack>>> il)
+            where TParameter : ITypeList
+            where TLocal : ITypeList
+            where TCallStack : ITypeList
+            => new IL<TParameter, TLocal, Stack<bool, TCallStack>>(new Op(OpCodes.Ceq), il);
+
+        public static IL<TParameter, TLocal, Stack<bool, TCallStack>> Cgt<T, TParameter, TLocal, TCallStack>(this IL<TParameter, TLocal, Stack<T, Stack<T, TCallStack>>> il)
+            where TParameter : ITypeList
+            where TLocal : ITypeList
+            where TCallStack : ITypeList
+            where T : struct
+            => new IL<TParameter, TLocal, Stack<bool, TCallStack>>(new Op(OpCodes.Cgt), il);
+
+        public static IL<TParameter, TLocal, Stack<bool, TCallStack>> Cgt_Un<T, TParameter, TLocal, TCallStack>(this IL<TParameter, TLocal, Stack<T, Stack<T, TCallStack>>> il)
+            where TParameter : ITypeList
+            where TLocal : ITypeList
+            where TCallStack : ITypeList
+            where T : struct
+            => new IL<TParameter, TLocal, Stack<bool, TCallStack>>(new Op(OpCodes.Cgt_Un), il);
+
+        public static IL<TParameter, TLocal, Stack<bool, TCallStack>> Clt<T, TParameter, TLocal, TCallStack>(this IL<TParameter, TLocal, Stack<T, Stack<T, TCallStack>>> il)
+            where TParameter : ITypeList
+            where TLocal : ITypeList
+            where TCallStack : ITypeList
+            where T : struct
+            => new IL<TParameter, TLocal, Stack<bool, TCallStack>>(new Op(OpCodes.Clt), il);
+
+        public static IL<TParameter, TLocal, Stack<bool, TCallStack>> Clt_Un<T, TParameter, TLocal, TCallStack>(this IL<TParameter, TLocal, Stack<T, Stack<T, TCallStack>>> il)
+            where TParameter : ITypeList
+            where TLocal : ITypeList
+            where TCallStack : ITypeList
+            where T : struct
+            => new IL<TParameter, TLocal, Stack<bool, TCallStack>>(new Op(OpCodes.Clt_Un), il);
     }
 }
