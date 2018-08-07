d93bc923136e16da7e08fffd78f54196a9c1534c
acple <silentsphere110@gmail.com>
2018-08-07 17:17:39 +0900

    Add conversion operators, wip

---
+++ TypedMethodBuilder/src/Builder/ILBuilder.Convert.cs
@@ -0,0 +1,66 @@
+using System.Reflection.Emit;
+
+namespace TypedMethodBuilder
+{
+    public static partial class ILBuilder
+    {
+        public static IL<TParameter, TLocal, Stack<sbyte, TCallStack>> Conv_I1<T, TParameter, TLocal, TCallStack>(this IL<TParameter, TLocal, Stack<T, TCallStack>> il)
+            where TParameter : ITypeList
+            where TLocal : ITypeList
+            where TCallStack : ITypeList
+            where T : struct
+            => new IL<TParameter, TLocal, Stack<sbyte, TCallStack>>(new Op(OpCodes.Conv_I1), il);
+
+        public static IL<TParameter, TLocal, Stack<short, TCallStack>> Conv_I2<T, TParameter, TLocal, TCallStack>(this IL<TParameter, TLocal, Stack<T, TCallStack>> il)
+            where TParameter : ITypeList
+            where TLocal : ITypeList
+            where TCallStack : ITypeList
+            where T : struct
+            => new IL<TParameter, TLocal, Stack<short, TCallStack>>(new Op(OpCodes.Conv_I2), il);
+
+        public static IL<TParameter, TLocal, Stack<int, TCallStack>> Conv_I4<T, TParameter, TLocal, TCallStack>(this IL<TParameter, TLocal, Stack<T, TCallStack>> il)
+            where TParameter : ITypeList
+            where TLocal : ITypeList
+            where TCallStack : ITypeList
+            where T : struct
+            => new IL<TParameter, TLocal, Stack<int, TCallStack>>(new Op(OpCodes.Conv_I4), il);
+
+        public static IL<TParameter, TLocal, Stack<long, TCallStack>> Conv_I8<T, TParameter, TLocal, TCallStack>(this IL<TParameter, TLocal, Stack<T, TCallStack>> il)
+            where TParameter : ITypeList
+            where TLocal : ITypeList
+            where TCallStack : ITypeList
+            where T : struct
+            => new IL<TParameter, TLocal, Stack<long, TCallStack>>(new Op(OpCodes.Conv_I8), il);
+    }
+
+    public static partial class ILBuilder
+    {
+        public static IL<TParameter, TLocal, Stack<byte, TCallStack>> Conv_U1<T, TParameter, TLocal, TCallStack>(this IL<TParameter, TLocal, Stack<T, TCallStack>> il)
+            where TParameter : ITypeList
+            where TLocal : ITypeList
+            where TCallStack : ITypeList
+            where T : struct
+            => new IL<TParameter, TLocal, Stack<byte, TCallStack>>(new Op(OpCodes.Conv_U1), il);
+
+        public static IL<TParameter, TLocal, Stack<ushort, TCallStack>> Conv_U2<T, TParameter, TLocal, TCallStack>(this IL<TParameter, TLocal, Stack<T, TCallStack>> il)
+            where TParameter : ITypeList
+            where TLocal : ITypeList
+            where TCallStack : ITypeList
+            where T : struct
+            => new IL<TParameter, TLocal, Stack<ushort, TCallStack>>(new Op(OpCodes.Conv_U2), il);
+
+        public static IL<TParameter, TLocal, Stack<uint, TCallStack>> Conv_U4<T, TParameter, TLocal, TCallStack>(this IL<TParameter, TLocal, Stack<T, TCallStack>> il)
+            where TParameter : ITypeList
+            where TLocal : ITypeList
+            where TCallStack : ITypeList
+            where T : struct
+            => new IL<TParameter, TLocal, Stack<uint, TCallStack>>(new Op(OpCodes.Conv_U4), il);
+
+        public static IL<TParameter, TLocal, Stack<ulong, TCallStack>> Conv_U8<T, TParameter, TLocal, TCallStack>(this IL<TParameter, TLocal, Stack<T, TCallStack>> il)
+            where TParameter : ITypeList
+            where TLocal : ITypeList
+            where TCallStack : ITypeList
+            where T : struct
+            => new IL<TParameter, TLocal, Stack<ulong, TCallStack>>(new Op(OpCodes.Conv_U8), il);
+    }
+}
